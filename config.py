9836e5438bc745b7f8a17d11c46cae88bf42f234
lixoffline90-droid <lixoffline90@gmail.com>
2026-09-24 13:48:25 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -10,3 +10,8 @@ MONGO_URI = os.getenv("MONGO_URI", "")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY", "")
 GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.3-70b-versatile")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+
+START_IMAGE_URL = os.getenv("START_IMAGE_URL", "")
+SUPPORT_URL = os.getenv("SUPPORT_URL", "")
+UPDATES_URL = os.getenv("UPDATES_URL", "")
+OWNER_URL = os.getenv("OWNER_URL", "")
