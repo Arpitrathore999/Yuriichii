0f6a3b0e653055ef4945f1ea7ac272ff85de2872
lixoffline90-droid <lixoffline90@gmail.com>
2026-09-24 13:59:34 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,15 @@ import asyncio
 from core.bot import app
 from core.loader import load_handlers
 
+
 async def main():
     load_handlers()
+
+    await app.start()
     print("Elara Bot started.")
+
     await asyncio.Event().wait()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
